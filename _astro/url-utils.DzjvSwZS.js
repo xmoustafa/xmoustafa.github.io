0b0f091d25ab201df42d03f722f582f03938e2cf
xmoustafa <36385656+xmoustafa@users.noreply.github.com>
2026-08-28 17:29:26 +0000

    Deploying to pages from @ xmoustafa/xmoustafa.github.io@0ba1c078d243f7d3a8b9edb4eca27bb2407286d6 🚀

---
+++ _astro/url-utils.DzjvSwZS.js
@@ -0,0 +1,1 @@
+import"./translation.1noma6xf.js";import"./config.COjaK3t5.js";function a(r,e){return r.replace(/^\/|\/$/g,"").toLowerCase()===e.replace(/^\/|\/$/g,"").toLowerCase()}function t(...r){return r.join("/").replace(/\/+/g,"/")}function u(r){return t("","/",r)}export{u as n,a as t};
